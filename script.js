7a189e6eb6dd75a7f2ea7da0a4a3c3ec9fee4000
Anojk123 <140962120+Anojk123@users.noreply.github.com>
2024-10-21 00:55:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let of =<i class="fa-sharp fa-regular fa-lightbulb" ></i>;
+let on=<i class="fa-sharp fa-solid fa-lightbulb"></i>;
+
+
+
+
+function lightOn(){
+    let turnOn=document.getElementById('bulb').on;
+};
+
+function lightOf(){
+    let turnOf=document.getElementById('bulb').of;
+};
